a7ba9d5bf11fe20c9752e41654e2ad46e7161795
ADITYApra <139861573+ADITYAzin@users.noreply.github.com>
2024-05-19 06:54:05 +0700

    change fa-regular to fa-solid

--- public/javascript/recommendProduct.js
+++ public/javascript/recommendProduct.js
@@ -1,8 +1,25 @@
 const whislist = document.querySelectorAll('.whislist');
+const loveIcon = document.querySelectorAll('.love-icon');
 whislist.forEach(index => {
     index.addEventListener('click', () => {
-        if(index.classList.contains('whislist')){
-            [index.classList.remove('fa-plus'), index.classList.add('fa-check')] = [index.classList.remove('fa-check'), index.classList.add('fa-plus')];
+        if(index.classList.contains('fa-regular')){
+            index.classList.remove('fa-plus');
+            index.classList.add('fa-check');
+        } else if(index.classList.contains('fa-check')){
+            index.classList.remove('fa-check');
+            index.classList.add('fa-plus');
+        }
+    });
+})
+
+loveIcon.forEach(indexlove => {
+    indexlove.addEventListener('click', () => {
+        if(indexlove.classList.contains('fa-plus')){
+            indexlove.classList.remove('fa-regular');
+            indexlove.classList.add('fa-solid');
+        } else if(index.classList.contains('fa-solid')){
+            indexlove.classList.remove('fa-solid');
+            indexlove.classList.add('fa-regular');
         }
     });
 })
